bbd108e9dbab33b98a9499260c3505901f41cf93
Sangyun Lee <116072804+sangyunlee-education@users.noreply.github.com>
2026-08-10 18:28:39 +0900

    Update 01_study1_exact_equivalence.R

--- R/simulation/01_study1_exact_equivalence.R
+++ R/simulation/01_study1_exact_equivalence.R
@@ -12,7 +12,7 @@ source(file.path("R", "simulation", "00_helpers.R"))
 
 N_REP <- 5000L
 CHUNK_SIZE <- 100L
-MASTER_SEED <- 20260810L
+MASTER_SEED <- 20260806L
 
 sample_sizes <- c(500L, 1000L, 2000L)
 distributions <- c("Normal", "Skewed", "Bimodal")
